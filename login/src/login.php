8e27629f28a3e28d0c134625cd811589c0fbda9e
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-03-26 09:50:23 -0300

    Correção das pastas e caminho do php

--- login/src/login.php
+++ login/src/login.php
@@ -4,6 +4,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = htmlspecialchars($_POST['user']);
     $pass = htmlspecialchars($_POST['pass']);
 
-    header('Location: /blog_pessoal/login/src/blog/');
+    header('Location: ../../blog/index.html');
     exit;
 }
